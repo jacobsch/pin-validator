21c439ade0173c9804adf837152ae0d8c6e80822
Jacob Schwartz <jacobschwartz@live.ca>
2019-04-14 16:21:08 -0400

    Finished adding comments

--- JahWindow.java
+++ JahWindow.java
@@ -10,16 +10,15 @@
 // Importing Required packages
 import java.awt.*; // Everything needed
 import javax.swing.*; // GUI
-import java.awt.event.*; // Making buttons work
+import java.awt.event.*; // Making buttons and more work
 import java.awt.event.ActionEvent; // Making buttons work
 import java.awt.event.ActionListener; // Making buttons work
 
 public class JahWindow extends JFrame {
   
-  // adding sister panels to the program
-	JPanel container;
-	Panel1 register;
-	Panel2 login;
+	JPanel container; // Adding JPanel 'container' to the main program
+	Panel1 register; // Adding Panel1 'register' to the main program
+	Panel2 login; // Adding Panel2 'login' to the main program
 
 	public JahWindow() {
 
@@ -33,14 +32,16 @@ public class JahWindow extends JFrame {
                 register = new Panel1(container); //init panels in the window
                 login = new Panel2(container); //init panels in the window
 
-                container.add(register, "Register");
-                container.add(login, "Login");
+                container.add(register, "Register"); //adding the JPanels to the container
+                container.add(login, "Login"); //adding the JPanels to the container
 
-                this.add(container);
-                this.setVisible(true);
+                this.add(container); //inserting the panels into the window
+                this.setVisible(true); //allowing the frame to actually be visible in the window
         }
 
         public static void main(String[] args) {
               new JahWindow(); //init the window, run the program
       }
 }
+// End of JahWindow
+// End of program
